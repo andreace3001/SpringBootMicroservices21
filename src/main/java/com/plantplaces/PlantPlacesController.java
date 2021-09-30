e2e5f4ddda881e10f0be4ae8f6921a6d32e363ed
andreace3001 <acebalao@mail.uc.edu>
2021-09-30 17:49:46 -0400

    Most up to date changes on environment set up. 9/30/2021 5:50 pm

--- src/main/java/com/plantplaces/PlantPlacesController.java
+++ src/main/java/com/plantplaces/PlantPlacesController.java
@@ -1,23 +1,58 @@
 package com.plantplaces;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
-/**
- * Handle the /start endpoint
- * @return
- * 
- */
+import com.plantplaces.dto.SpecimenDTO;
+import com.plantplaces.service.SpecimenServiceStub;
+
+
+
+
 @Controller
 public class PlantPlacesController {
+	
+	@Autowired
+	private SpecimenServiceStub specimenServiceStub;
 
 	@RequestMapping(value="/start", method=RequestMethod.GET)
-	public String read(){
+	public String read(Model model){
+		
+		SpecimenDTO specimenDTO = specimenServiceStub.fetchBy(43);
+		model.addAttribute("specimenDTO", specimenDTO);
 		
 		return "start";
 	}
+	
+	@RequestMapping(value="/start", method=RequestMethod.GET,headers= {"content-type=text/json"})
+	public String readJson(){
+		
+		return "start";
+	}
+	
+	@RequestMapping(value="/start", method=RequestMethod.GET, params = {"loyalty=blue"})
+	public String readBlue(){
+		
+		return "start";
+	}
+	
+	@RequestMapping(value="/start", method=RequestMethod.GET, params = {"loyalty=silver"})
+	public ModelAndView readSilver(){
+		
+		SpecimenDTO specimenDTO = specimenServiceStub.fetchBy(43);
+		specimenDTO.setSpecimenId(85);
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("start");		
+		modelAndView.addObject("specimenDTO", specimenDTO);
+		return modelAndView;
+	}
+	
+	
 	
 	@PostMapping("/start")
 	public String create(){
@@ -26,7 +61,7 @@ public class PlantPlacesController {
 	}
 	
 	/**
-	 * Handle the endpoint
+	 * Handle the /index endpoint
 	 * @return
 	 * 
 	 */
